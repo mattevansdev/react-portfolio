28f2425b805f2a0df5f23a03bf901a129872450b
Matthew Evans <m@iammatthew.com>
2021-10-30 13:54:32 -0700

    Menu

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,25 @@
+import "./topbar.scss"
+import { Mail } from '@material-ui/icons';
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+    return (
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">Chela Evans</a>
+                    <div className="itemContainer">
+                        <Mail className="icon" />
+                        <span>chela@chelaevans.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
